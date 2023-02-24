e8d6a6c4b0f4656006e376fa4b87660cd7483130
Aditya27T <aditpoetra25@gmail.com>
2023-02-24 07:39:38 +0700

    some update

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ const quiz = require('./quiz')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'English-API' });
 });
 
 /* Quiz page. */
